6d6ba400675efb4155bc650be1a9947dc2a966a7
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-08-25 01:06:35 +1000

    Task 1-5 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,67 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const result = books[req.params.isbn];
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: 'Book not found' });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const results = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].author.toLowerCase() === author) {
+      results.push(books[key]);
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(results);
+  } else {
+    res.status(404).send({ error: 'No books found for this author' });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const results = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].title.toLowerCase() === title) {
+      results.push(books[key]);
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(results);
+  } else {
+    res.status(404).send({ error: 'No books found with this title' });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    res.send(book.reviews);
+  } else {
+    res.status(404).send({ error: 'Book not found' });
+  }
 });
 
 module.exports.general = public_users;
