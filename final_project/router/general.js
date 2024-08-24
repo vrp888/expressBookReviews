461a0b4fbe353100378dee71f33217e46646ef0e
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-08-25 02:06:56 +1000

    Task 7 - 9 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,6 +1,5 @@
 const express = require('express');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
