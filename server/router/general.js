709517ae74bdd0ea65a161e7e3097580ed05c82b
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-08-25 02:11:50 +1000

    Rename final_project to server + create client

---
+++ server/router/general.js
@@ -0,0 +1,104 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+      return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  res.send(JSON.stringify(books,null,4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  const result = books[req.params.isbn];
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: 'Book not found' });
+  }
+});
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  const author = req.params.author.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const results = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].author.toLowerCase() === author) {
+      results.push(books[key]);
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(results);
+  } else {
+    res.status(404).send({ error: 'No books found for this author' });
+  }
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  const title = req.params.title.toLowerCase();
+  const bookKeys = Object.keys(books);
+  const results = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].title.toLowerCase() === title) {
+      results.push(books[key]);
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(results);
+  } else {
+    res.status(404).send({ error: 'No books found with this title' });
+  }
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    res.send(book.reviews);
+  } else {
+    res.status(404).send({ error: 'Book not found' });
+  }
+});
+
+module.exports.general = public_users;
